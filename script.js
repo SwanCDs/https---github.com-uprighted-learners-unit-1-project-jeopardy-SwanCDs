9bd3f4dfdf8a4ff996ebadd1d68e04c332dd677d
Peter Mbucho <realdoonmug@gmail.com>
2024-12-16 07:45:43 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const playeronepoints = 0;
+const playertwopoints = 0;
+
+if(finalanwser === "26") {
+    return console.log("right anwser")
+    if(playeronepoints) {
+        playeronepoints + 500
+    } else {
+        playertwopoints + 500
+    }
+} else {
+    console.log("wrong anwser")
+    if(playeronepoints) {
+        playeronepoints + 500
+    } else {
+        playertwopoints + 500
+    }
+}
